5c671f44e5eba3d3304f929d9b7a44cf7d4f3296
Kuxtal <japs.8205@gmail.com>
2015-10-07 18:32:37 -0500

    Actualizacion de url

--- src/main/webapp/recursos/js/services.js
+++ src/main/webapp/recursos/js/services.js
@@ -18,7 +18,7 @@
 var services = angular.module('app.services', ['ngResource']);
 
 services.factory('MenuFactory', function ($resource, ServiciosGlobales) {
-    return $resource(ServiciosGlobales.API_URL + '/moduloes/menuJson?idPortal=:idPortal', {}, {
+    return $resource(ServiciosGlobales.API_URL + '/portals/:idPortal/menu', {}, {
         query: { method: 'GET', isArray: true, cache: false },
         findPortalMenu: { method: 'GET', isArray: true, cache: false, params: {idPortal: '@idPortal'} }
     })
@@ -26,7 +26,7 @@ services.factory('MenuFactory', function ($resource, ServiciosGlobales) {
 
 services.factory('ServiciosGlobales', function() {
 	return {
-		API_URL : 'http://applocal\\:8080/pruebatoken-0.0.1-SNAPSHOT/api',
+		API_URL : 'http://applocal\\:8080/configuracion',
 		SSO_URL : 'http://applocal\\:8080/configuracion',
 		PORTAL_URL : 'http://applocal\\:8080/configuracion'
 	};
